a41c8cd9fbcb721f75b53e393cfeb04852984842
Fabian Morawetz <fabian.morawetz@edag.de>
2017-06-29 15:26:53 +0200

    added form inputs

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -4,22 +4,48 @@ import { PrimaryButton, Form } from './common';
 
 class LoginComponent extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
   login(username, password) {
     this.props.fetchLogin(username, password);
-  };
+  }
 
   goToRegistrationScreen() {
     alert('Registrieren');
-  };
+  }
 
   goToForgotPasswordScreen() {
     alert('Passwort zurücksetzen');
-  };
+  }
+
+  onChangeUsernameText(username) {
+    this.setState({
+      username: username
+    });
+  }
 
   render() {
-      return(
-        <View/>
-      );
+    return (
+      <View>
+        <Form
+          titleText={'username'}
+          placeholder={'username'}
+          onChangeText={() => this.onChangeUsernameText}
+          value={this.state.username}
+        />
+        <Form
+          titleText={'password'}
+          secureTextEntry
+          placeholder={'password'}
+        />
+      </View>
+    );
   }
 }
 
